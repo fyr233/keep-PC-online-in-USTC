ce407dd71fabc4dd82337438894500e34741f73e
fyr233 <36994804+fyr233@users.noreply.github.com>
2019-05-16 17:51:32 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,89 @@
+import requests
+import psutil
+import socket
+import time
+import json
+import re
+import os
+
+ip_url = 'http://47.94.255.161:1925/addlog'
+RTdata_url = 'http://localhost:8085/data.json'
+wlt_url = 'http://wlt.ustc.edu.cn/cgi-bin/ip'
+
+def parse_RTdata(j):
+    cpu = ' '.join(re.findall('"Text": "CPU Core .*? "Value": "(.*?)",',j))
+    gpu = ' '.join(re.findall('"Text": "Temperatures", .*? "Text": "GPU Core", .*? "Value": "(.*?)",',j))
+    gpu += ' ' + re.findall('"Text": "Fans", .*? "Text": "GPU Fan", .*? "Value": "(.*?)",',j)[0]
+    return cpu,gpu
+
+#开通网络通
+def open_wlt():
+    print('正在设置网络通')
+    #获取校内ip
+    req1 = requests.get(wlt_url)
+    req1.encoding = 'gb2312'
+    schoolIP = re.findall('<td width=290>(.*?) </td>', req1.text)[0]
+    #登录
+    login_data = {
+        'cmd': 'login',
+        'url': 'URL',
+        'name': 'fyr233',
+        'ip': schoolIP,
+        'password': '233016',
+        'savepass': 'on',
+        'go': '%B5%C7%C2%BC%D5%CA%BB%A7'
+        }
+    req2 = requests.post(wlt_url, data=login_data)
+    cookie_rn = req2.headers['Set-Cookie']#获取rn
+    #开通网络
+    set_url = wlt_url + '?cmd=set&url=URL&type=0&exp=0&go=+%BF%AA%CD%A8%CD%F8%C2%E7+'#type为出口，exp为时长
+    set_headers = {
+                'Host': 'wlt.ustc.edu.cn',
+                'Referer': 'http://wlt.ustc.edu.cn/cgi-bin/ip',
+                'Cookie': 'name=fyr233; password=233016; ' + cookie_rn,
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.131 Safari/537.36'
+                }
+    req3 = requests.get(set_url, headers=set_headers)
+    req3.encoding = 'gb2312'
+    set_info = re.findall('<td>(信息.*?)<p>', req3.text)[0]
+    print(set_info)
+
+def set_WiFi():
+    print('正在连接eduroam')
+    os.system('netsh wlan connect name=eduroam ssid=eduroam interface="WLAN"')#连接eduroam
+
+time.sleep(10)
+while True:
+    #cpu_usage = psutil.cpu_percent()
+    hostname = socket.gethostname()
+    IP = socket.gethostbyname(hostname)
+    #print(cpu_usage)
+    try:
+        r2 = requests.get(RTdata_url)
+        RTdata = parse_RTdata(r2.text)
+        r2t = r2.text
+    except:
+        RTdata = (str(psutil.cpu_percent()), 'NOdata')
+        r2t = '数据获取失败'
+
+    data = {'ip':IP,
+        'cpu':RTdata[0],
+        'gpu':RTdata[1],
+        'RTdata':r2t}
+    try:
+        r1 = requests.post(ip_url, data=data)
+    except:
+        print('network error')
+        try:
+            open_wlt()
+        except:
+            print('网络通也上不去啦')
+            set_WiFi()
+            time.sleep(15)
+            try:
+                open_wlt()
+            except:
+            	pass
+    else:
+        print('success')
+    time.sleep(30)
